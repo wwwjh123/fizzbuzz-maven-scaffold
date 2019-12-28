bf1aafa853ee75249293a6942d11d14214e0c5fc
wwwjh123 <271513982@qq.com>
2019-12-28 11:07:08 +0800

    refactor

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -4,15 +4,18 @@ import java.util.List;
 public  class FizzBuzz {
     public static List<String> FizzBuzz(int n){
         List<String> list = new ArrayList<String>();
-        for (int i=1; i<= n; i++) {
-            if (i%3 != 0) {
-                if (i%5 != 0) { list.add(i+"");
-                } else { list.add("Buzz"); }
+        for (int i=1; i<= n; i++)
+            if (i % 3 != 0) {
+                if (i % 5 != 0) {
+                    list.add(String.format("%d", i));
+                } else {
+                    list.add("Buzz");
+                }
             } else {
-                if (i%5 != 0) { list.add("Fizz");
-                } else { list.add("FizzBuzz"); }
+                if (i % 5 != 0) list.add("Fizz");
+                else list.add("FizzBuzz");
             }
-        } return list;
+        return list;
     }
 
     public static void main(String[] args) {
