9299c757275fe709b71d76f673907b9b161a05af
wwwjh123 <271513982@qq.com>
2019-12-28 10:26:21 +0800

    refactor

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public  class FizzBuzz {
+    public static List<String> FizzBuzz(int n){
+        List<String> list = new ArrayList<String>();
+        for (int i=1; i<= n; i++) {
+            if (i%3 != 0) {
+                if (i%5 != 0) { list.add(i+"");
+                } else { list.add("Buzz"); }
+            } else {
+                if (i%5 != 0) { list.add("Fizz");
+                } else { list.add("FizzBuzz"); }
+            }
+        } return list;
+    }
+
+    public static void main(String[] args) {
+        int n=100;
+        System.out.println(FizzBuzz.FizzBuzz(n).toString());
+    }
+
+}
